90f454401ff144cc07cc5db229900ca2cb931e03
Glen Shewchuck <glenshewchuck@gmail.com>
2020-06-03 08:11:55 -0700

    position the photo information after the compare image

--- web/themes/custom/chewch/js/before-and-after.js
+++ web/themes/custom/chewch/js/before-and-after.js
@@ -1,6 +1,8 @@
 jQuery(function(){
+
+  // Now show the image the way we want
   jQuery(".twentytwenty-container").twentytwenty({
-    default_offset_pct: 0.2, // How much of the before image is visible when the page loads
+    default_offset_pct: 0.1, // How much of the before image is visible when the page loads
     orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
     before_label: 'Before Post', // Set a custom before label
     after_label: 'After Post', // Set a custom after label
@@ -9,4 +11,9 @@ jQuery(function(){
     move_with_handle_only: true, // Allow a user to swipe anywhere on the image to control slider movement.
     click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
   });
+
+  // Now we must move the photo information because it gets hidden under the
+  // photo that is now absolutely positioned with no overflow.
+  var bootstrapPanel = jQuery('#bootstrap-panel').detach();
+  bootstrapPanel.appendTo(jQuery('#container-image-before-after'));
 });
